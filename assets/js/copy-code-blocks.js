f068ae7bf751289b6cb086ec73ebe7a5cf689abf
Adam Porter <adam@alphapapa.net>
2023-11-30 12:16:54 -0600

    Change: Use PST's files more directly
    This uses the related Sass and CSS files from PST directly and
    overrides properties as necessary in "copybutton.local.scss".

--- assets/js/copy-code-blocks.js
+++ assets/js/copy-code-blocks.js
@@ -6,21 +6,19 @@
 // This selector should match the PRE elements, each of which may
 // itself contain the text to be copied, or may contain a CODE element
 // which contains the text to be copied.
-const pre_selector = "div.highlight pre";
+const div_selector = "div.highlight";
 
-document.querySelectorAll(pre_selector).forEach(add_button_to);
+document.querySelectorAll(div_selector).forEach(add_button_to);
 
 function add_button_to(element) {
-  div = document.createElement("div");
-  button = document.createElement("button");
-  div.classList.add("copy-button");
+  const button = document.createElement("button");
+  button.classList.add("copybtn");
   button.addEventListener("click", copy_content_of);
-  div.append(button);
-  element.prepend(div);
+  element.append(button);
 }
 
 function copy_content_of(event) {
-  content = this.parentElement.parentElement.textContent + "\n";
+  const content = this.parentElement.parentElement.textContent + "\n";
   navigator.clipboard.writeText(content).then(() => update_button(this));
 }
 
